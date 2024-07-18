618732e8135b4450580d78b71209b603b1cce10e
Hari Prasad <haririo321@gmail.com>
2024-07-18 11:57:50 +0530

    changes

---
+++ attendance_app_bma/lib/models/student_model.dart
@@ -0,0 +1,12 @@
+class Student {
+  final int id;
+  final DateTime createdAt;
+  final String name;
+  final String email;
+
+  Student(
+      {required this.id,
+      required this.createdAt,
+      required this.name,
+      required this.email});
+}
